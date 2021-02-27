9aaf512eb579f0c866a671f4b15126d33af730b4
LG <sjh8236@naver.com>
2021-02-27 18:38:45 +0900

    커밋

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_Instance; //유일성 보장
+    public static Managers Instance { get { Init(); return s_Instance; } } // 유일한 매니저를 갖고온다.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    static void Init()
+    {
+        if (s_Instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_Instance = go.GetComponent<Managers>();
+
+        }
+    }
+}
